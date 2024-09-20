6854d192f2d478ab6c288cb1a76b1ad06480ba2b
Sunil K <sunilk.offsureit@gmail.com>
2024-09-20 16:41:46 +0530

    Jitpack publishing configured

--- CustomAlert/build.gradle.kts
+++ CustomAlert/build.gradle.kts
@@ -25,14 +25,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         viewBinding = true
+    }
+}
+
+publishing{
+    publications{
+        register<MavenPublication>("release"){
+            afterEvaluate{
+                from(components["release"])
+            }
+        }
     }
 }
 
